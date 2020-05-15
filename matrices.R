65d89d68ef3b606ef9432b941998121bcba15f19
Alex <26582461+aheggie@users.noreply.github.com>
2020-05-15 19:02:01 -0400

    max_of pulled out to generic function function_of

--- matrices.R
+++ matrices.R
@@ -1,19 +1,28 @@
 d3 <- 1:3
 
-max_of <- function(row_rolls, col_rolls) {
+#function of compares one vector of rolls with another via some comparison function f
+function_of <- function(row_rolls, col_rolls, f) {
   mat <- matrix(0, nrow = length(row_rolls), ncol = length(col_rolls), dimnames = list(row_rolls, col_rolls))
   
   #r for rows
   for(r in 1:length(row_rolls)) {
     #c for cols
     for(c in 1:length(col_rolls)) {
-      mat[r,c] <- max(row_rolls[r], col_rolls[c])
+      mat[r,c] <- f(row_rolls[r], col_rolls[c])
     }
   }
   mat
 }
 
-mat <- max_of(d3, d3)
+max_of <- function(row_rolls, col_rolls) {
+  function_of(row_rolls, col_rolls, max)
+}
+
+advantage <- function(rolls) {
+  max_of(rolls, rolls)
+}
+
+mat <- advantage(d3)
 
 mat[1, 1]
 mat[1, 2]
